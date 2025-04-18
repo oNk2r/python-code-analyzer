2022a98b0fdb94c9c4a9634e54b7b8ce837f7efa
Onkar Tulshigar <ort.200516@gmail.com>
2025-04-18 16:09:08 +0530

    Add files via upload

---
+++ sample_code.py
@@ -0,0 +1,12 @@
+# Sample Python file
+
+def greet(name):
+    # This function greets the user
+    print(f"Hello, {name}!")
+
+def long_function():
+    total = 0
+    for i in range(100):
+        total += i
+    print("Done")
+    return total
